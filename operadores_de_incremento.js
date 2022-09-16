0ad8b3474d0e2fdc750e51a5d6d6892467960458
danielmurilo <danielmurilo@yahoo.com.br>
2022-09-16 17:08:29 -0300

    operadores de incremento e decremento

--- operadores_de_incremento.js
+++ operadores_de_incremento.js
@@ -1,2 +1,7 @@
 const nomeUsuario = prompt('Olá, qual seu nome?')
 alert('seja bem vindo '+ nomeUsuario)// mostrar msg ao usuário
+const num = prompt('Digite um número:')
+alert('o numero digitado é: '+ num)
+const aux = num
+alert('o sucessor do numero digitado é: '+ ++num)
+alert('o antecessor do numero digitado é: '+ --aux)
