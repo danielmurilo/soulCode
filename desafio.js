45273446b3da8ee50ec15866adbf79fcbe202947
danielmurilo <danielmurilo@yahoo.com.br>
2022-09-21 16:56:49 -0300

    aula IF

---
+++ desafio.js
@@ -0,0 +1,35 @@
+/**
+ * Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço
+normal de etiqueta e a escolha da condição de pagamento. Utilize os códigos da tabela a seguir
+para ler qual a condição de pagamento escolhida e efetuar o cálculo adequado.
+Código Condição de pagamento
+
+1 À vista em dinheiro ou cheque, recebe 10% de desconto
+2 À vista no cartão de crédito, recebe 15% de desconto
+3 Em duas vezes, preço normal de etiqueta sem juros
+4 Em três vezes, preço normal de etiqueta mais juros de 10%
+ */
+
+const precoProduto = parseFloat(prompt('Qual o preço do produto?'))
+const mensagem = `
+Qual é a forma de pagamento? Digite:
+1 Dinheiro
+2 Cheque
+3 Cartão`
+const formaPagamento = prompt(mensagem)
+
+if(formaPagamento == '1' || formaPagamento == '2'){
+    const precoFinal = precoProduto * 0.9
+    alert(`Valor a Pagar: R$ ${precoFinal.toFixed(2)}`)
+} else if (formaPagamento == '3'){
+    const parcelas = parseInt(prompt('Informe a Quantidade de Parcelas'))
+    if (parcelas == '1') {
+        const precoFinal = precoProduto * 0.85
+        alert(`Valor a Pagar: R$ ${precoFinal.toFixed(2)}`)
+    } else if (parcelas == '2') {
+        alert(`Valor a Pagar: R$ ${precoFinal.toFixed(2)}`)
+    } else (parcelas >= '3') {
+        const precoFinal = precoProduto * 1.1
+        alert(`Valor a Pagar: R$ ${precoFinal.toFixed(2)}`)
+    }
+}
--- strings.js
+++ strings.js
@@ -16,7 +16,7 @@ com
 console.log(stringComCrase)
 
 //metodos das strings
-const texto = 'O Javascript foi criado alguns anos após os Python. No entanto, o Javascript é bem mais bacana.'
+const texto = 'O Javascript foi criado alguns anos após o Python. No entanto, o Javascript é bem mais bacana.'
 console.log('texto original: ' + texto)
 console.log('toUpperCase(): ' + texto.toUpperCase())
 console.log('toLowerCase(): ' + texto.toLowerCase())
@@ -26,7 +26,7 @@ console.log("replaceAll('o','0'): " + texto.replaceAll('o','0'))//substitui toda
 console.log('substring(2,12): ' + texto.substring(2,12))
 console.log('Regex: ' + texto.replace(/Javascript/g, 'PHP')) //case sensitive
 console.log('Regex: ' + texto.replace(/Javascript/gi, 'PHP')) //no case sensitive
-//console.log('' + texto)
+console.log('' + texto.replace(/[\s]/gi, '_')) //muda todos os espaços em branco por underline
 
 let html = '<!DOCTYPE html>\n<html lang="pt-br">\n\<head>'
 console.log(html.startsWith('<!DOCTYPE html>'))
